cd3600994eaa398850913811bf111a314d870833
sandy <sandylai0116@gmail.com>
2015-06-10 00:31:12 +0800

    put the test case from Home class to computation class

--- app/src/main/java/com/project/tos_project/Home.java
+++ app/src/main/java/com/project/tos_project/Home.java
@@ -33,9 +33,6 @@ public class Home extends ActionBarActivity{
 
         card1 =(ImageButton) findViewById(R.id.card1);
 
-        //create 6 card
-        Card card[] = new Card[6];
-
    /*     card1.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v){
           //      Toast.makeText(getApplicationContext(), "Hello ",
@@ -59,30 +56,9 @@ public class Home extends ActionBarActivity{
             }
         });
 */
-        //assume card NO is 1
-        //assume current level is 5
-        for(int i=0;i<6;i++) {
-            card[i] = db.getCard(1);
-            card[i].setCurrentLevel(5);
-        }
 
-        //assume combo is 3
-        int combo =3;
-
-        //calculate leader skill
-        LeaderSkill.processLeaderSkill(card);
-
-        //calculate final attack
-        int finalAttack = 0;
-        for(int i=0;i<6;i++) {
-            finalAttack += card[i].getCalculatedAttack();
-        }
-
-        //get card
-        System.out.println(card[0].getCalculatedAttack() + "," + card[1].getCalculatedAttack() + "," + card[2].getCalculatedAttack() + ","
-                + card[3].getCalculatedAttack() + "," + card[4].getCalculatedAttack() + "," + card[5].getCalculatedAttack());
-        System.out.println(finalAttack);
-
+        //test
+        Computation.testFinalAttack(db);
 
         //close db
         db.closeDB();
